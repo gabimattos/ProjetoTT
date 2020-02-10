8f2c3a8c3fdabf78d263d9610d7127d92c768948
Gabriel Rodrigues Nunes <grnunes126@gmail.com>
2020-02-10 18:14:55 -0300

    +cadastro melhorado, formatacao perfil vendedor melhorada

--- Backend/BD/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/BD/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('state_id');
+            $table->string('state')->default('estado');
             $table->integer('delivery_price')->default('8');
             $table->string('photo')->nullable();
             $table->rememberToken();
